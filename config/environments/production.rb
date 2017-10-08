0a8e9a54c4149cf5ac7eef22171240497baee560
hereismyusername <my.e.address123@gmail.com>
2017-10-08 17:08:52 +0000

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'xxx.herokuapp.com' }
-ecedence over those in config/application.rb.
+  # Settinggs specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
