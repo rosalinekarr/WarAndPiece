2eacf4c56d88507fd695e5d5ec7bad3445ac023c
hereismyusername <hereismyusername@users.noreply.github.com>
2017-10-09 14:16:29 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'xxx.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'war-and-piece.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
