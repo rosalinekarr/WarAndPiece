4e61a8175940e0abc72c500daed26b4144461315
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-10-10 10:31:21 -0400

    merged master branch to incorporate devise gem so User model could be generated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'war-and-piece.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
