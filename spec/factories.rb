f2b9157177aa3575fb615468ca978197c156e210
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-11 18:57:54 +0000

    initial pass at tests for future red green refactor, and factories

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :move do |f|
+    f.game_id "1"
+    f.piece_id "1"
+    f.rank "1"
+    f.file "1"
+  end
+end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe Move, type: :model do
   it "has a valid factory" do
     expect(FactoryGirl.create(:move)).to be_valid
   end
-  # it "is invalid without a game_id" do
-  #   expect(FactoryGirl.build(:move, game_id: nil).valid?).to be false
-  # end
-  # it "is invalid without a piece_id" do
-  #   expect(FactoryGirl.build(:move, piece_id: nil).valid?).to be false
-  # end
-  # it "is invalid without a rank" do
-  #   expect(FactoryGirl.build(:move, rank: nil).valid?).to be false
-  # end
-  # it "is invalid without a file" do
-  #   expect(FactoryGirl.build(:move, file: nil).valid?).to be false
-  # end
+  it "is invalid without a game_id" do
+    expect(FactoryGirl.build(:move, game_id: nil).valid?).to be false
+  end
+  it "is invalid without a piece_id" do
+    expect(FactoryGirl.build(:move, piece_id: nil).valid?).to be false
+  end
+  it "is invalid without a rank" do
+    expect(FactoryGirl.build(:move, rank: nil).valid?).to be false
+  end
+  it "is invalid without a file" do
+    expect(FactoryGirl.build(:move, file: nil).valid?).to be false
+  end
 end
