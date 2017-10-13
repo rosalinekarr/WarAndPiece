6d3859f4ccfa6eb9d2820b69e51a15cde87d451e
hereismyusername <my.e.address123@gmail.com>
2017-10-13 13:30:03 +0000

    add way to update games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,15 @@ class GamesController < ApplicationController
     @games = Game.all
   end
 
+  def update
+    @game = Game.find(params[:id])
+    @game.update_attributes(game_params)
+    redirect_to_root_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(xxxx)
+  end
 end
