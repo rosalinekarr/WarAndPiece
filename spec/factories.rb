0e339b7f1ad82bd14f49e148ac7ddb809308e127
Renee Sandefur <reneersandefur@gmail.com>
2017-10-13 14:26:50 +0000

    added user factory, added tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+    factory :user do
+      sequence :email do |n|
+        "dummyEmail#{n}@gmail.com"
+      end
+      password "secretPassword"
+      password_confirmation "secretPassword"
+    end
   factory :game do
   end
   
