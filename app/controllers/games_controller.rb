290129e287be9800186962b9fa660127886c885c
Renee Sandefur <reneersandefur@gmail.com>
2017-10-13 14:36:34 +0000

    added line to rspec for devise, added methods create, new, game params, added new template

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,21 @@
 class GamesController < ApplicationController
-
+  before_action :authenticate_user!
   def index
+    #will the index show all games or just the current game? - RRS
     @games = Game.all
   end
-
+  
+  def new
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.create(game_params)
+  end
+  
+  def private
+    def game_params 
+      params.require(:game).permit(:game_state)
+    end
+  end
 end
