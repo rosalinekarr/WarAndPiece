5c16ebad8a9e136decaca9927861a5f3297fe6c2
Renee Sandefur <reneersandefur@gmail.com>
2017-10-13 14:13:28 +0000

    starting new game_controller branch

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
+  describe "games#index action" do
+    it "should require player to be logged in" do
+      get :new 
+      expect(response).to redirect_to new_user_session_path
+    end
+    it "should successfully show game" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "games#new action" do
+    it "should require player to be logged in" do
+      get :new 
+      expect(response).to redirect_to new_user_session_path
+    end
+    it "should successfully show form for new game" do
+    #a popup/modal asking if the player would like to start a new game
+      player = FactoryGirl.create(:user)
+      sign_in player
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+  
+  describe "games#create action" do
+    it "should successfully create a new game in our database" do
+      player = FactoryGirl.create(:user)
+      sign_in player
+      post :create, params: { game: { game_state: "new game" } }
+      expect(response).to redirect_to root_path
+      
+      game = Game.last
+      expect game.game_state.to eq("new game")
+      expect game.user.to eq(player)
+    end
+  end
 end
