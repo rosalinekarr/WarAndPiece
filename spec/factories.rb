795516f5d882e1609a35881c47068d61a0492e7d
Gary Ravago <ravagog@gmail.com>
2017-10-14 11:53:43 -0700

    Add Piece factory and tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
+  factory :user do
+      sequence :email do |n|
+        "dummyEmail#{n}@gmail.com"
+      end
+      password "secretPassword"
+      password_confirmation "secretPassword"
+  end
+
   factory :game do
   end
   
@@ -7,5 +15,13 @@ FactoryGirl.define do
 #     f.piece_id "1"
 #     f.rank "1"
 #     f.file "1"
+  end
+
+  factory :piece do     
+#    user
+#    game
+#    type     ["pawn","rook","knight","bishop","king","queen"].sample
+#    rank     (1..8).to_a.sample
+#    file     ('a'..'f').to_a.sample
   end
 end
