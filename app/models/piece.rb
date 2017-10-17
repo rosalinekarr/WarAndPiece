ef8632c65514c6fe86d048365514936fc3e8450d
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-17 12:41:08 +0000

    drafting is_obstructed method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,8 +4,14 @@ class Piece < ApplicationRecord
   belongs_to :black_player
   belongs_to :white_player
 
-  def is_obstructed
-
+  def is_obstructed(row, col)    ## rank and file of the square we want to move to
+    Piece.game.pieces each do |p|
+      if p.rank == row && p.file == col
+        return true
+      else
+        false
+      end
+    end
   end
 
 end
