95ab16527a59d81394fe739494008e5c6d05818d
hereismyusername <my.e.address123@gmail.com>
2017-10-17 16:05:07 +0000

    add authenticated and unauthenticated landing pages to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'games#index'
-  resource :games
+
+  authenticated :user do
+    root 'loggedin#index', as: :authenticated_root
+  end
+
+  root 'loggedout#index'
+  resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
