07640abc8f95dc249805f16cbd648498dd572080
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-18 13:48:13 +0000

    saving current draft

--- db/schema.rb
+++ db/schema.rb
@@ -65,4 +65,5 @@ ActiveRecord::Schema.define(version: 20171015173056) do
 
   add_foreign_key "games", "users", column: "black_player_id"
   add_foreign_key "games", "users", column: "white_player_id"
+  add_foreign_key "pieces", "users"
 end
