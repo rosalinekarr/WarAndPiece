be60cd31d3e3ade4086c133a9a375dda9a49eb5c
hereismyusername <my.e.address123@gmail.com>
2017-10-19 11:51:20 +0000

    add a way to join games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,13 @@
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show, :update]
+  before_action :authenticate_user!, only: [:new, :create, :show, :update, :join]
   def index
     @games = Game.all
   end
-  
+
   def new
     @game = Game.new
   end
-  
+
   def create
     @game = Game.create(game_params)
     redirect_to root_path
@@ -17,19 +17,25 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def join
+    @game = Game.find(params[:id])
+    @game.update_attributes(@game.black_player_id = current_user.id)
+    redirect_to join_games_path
+  end
+
   def update
     @game = Game.find(params[:id])
     @game.update_attributes(game_params)
     redirect_to root_path
   end
-  
+
   def destroy
     @game = Game.find(params[:id])
       return not_found(:forbidden) if @game.user != current_user
     @game.destroy
     redirect_to root_path
-  end  
-  
+  end
+
   private
 
   def game_params
