b15e5c6225d2185d32381b6449313cb9639fede3
Renee Sandefur <reneersandefur@gmail.com>
2017-10-22 02:31:24 +0000

    updated game controller show tests to pass by adding user login via Factory Girl to test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -29,7 +29,14 @@ RSpec.describe GamesController, type: :controller do
   end
 
   describe "games#show action" do
+    it "should require player to be logged in" do
+      get :new 
+      expect(response).to redirect_to new_user_session_path
+    end
     it "should successfully show the game if the game is found" do
+      player = FactoryGirl.create(:user)
+      sign_in player
+      
       game = FactoryGirl.create(:game)
       get :show, params: { id: game.id }
       puts response.body
@@ -37,6 +44,9 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it "should return a 404 error if the game is not found" do
+      player = FactoryGirl.create(:user)
+      sign_in player
+      
       get :show, params: { id: 'notreal' } 
       puts response.body
       expect(response).to have_http_status(:not_found)
