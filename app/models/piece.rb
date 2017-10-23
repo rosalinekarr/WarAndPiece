1823eeda65083365a6f1a92dbd88709aa39b92dc
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-23 11:09:26 +0000

    refactored to (x, y) axis

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,45 +3,42 @@ class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  def is_obstructed?(row, col)     ## pass in rank and file of the square we want to move to
-    current_row = self.rank     ## rank of the Piece we're applying the method to
+  def is_obstructed?(col, row)     ## pass in rank and file of the square we want to move to
     current_col = self.file     ## file of the Piece we're applying the method to
-    ranks = []
-    files = []
-    if current_col == col                                ## checks vertically
-      if current_row < row                               ## above
-        ranks = (current_row+1..row-1).map { |n| n = n }
-      else                                               ## below
-        ranks = (row+1..current_row-1).map { |n| n = n }
-      end
-      files = [col] * ranks.length
-    elsif current_row == row                             ## checks horizontally
+    current_row = self.rank     ## rank of the Piece we're applying the method to
+    if current_row == row                                ## checks horizontally
       if current_col < col                               ## to the right
         files = (current_col+1..col-1).map { |n| n = n }
       else                                               ## to the left
         files = (col+1..current_col-1).map { |n| n = n }
       end
       ranks = [row] * files.length
-    elsif (row - current_row) == (col - current_col)      ## check diagonally
-      if row - current_row > 0                            ## top-right
+    elsif  current_col == col                            ## checks vertically
+      if current_row < row                               ## above
         ranks = (current_row+1..row-1).map { |n| n = n }
-        files = (current_col+1..col-1).map { |n| n = n }
-      else                                                ## bottom-left
+      else                                               ## below
         ranks = (row+1..current_row-1).map { |n| n = n }
+      end
+      files = [col] * ranks.length
+    elsif (row - current_row) == (col - current_col)      ## check diagonally
+      if (row - current_row) > 0                          ## top-right
+        files = (current_col+1..col-1).map { |n| n = n }
+        ranks = (current_row+1..row-1).map { |n| n = n }
+      else                                                ## bottom-left
         files = (col+1..current_col-1).map { |n| n = n }
+        ranks = (row+1..current_row-1).map { |n| n = n }
       end
     else (row + col) == (current_row + current_col)
       if row > current_row                                ## top-left
+        files = (col+1..current_col-1).map { |n| n = n }        
         ranks = (current_row+1..row-1).map { |n| n = n }
-        files = (col+1..current_col-1).map { |n| n = n }
       else                                                ## bottom-right
-        ranks = (row+1..current_row-1).map { |n| n = n }
         files = (current_col+1..col-1).map { |n| n = n }
+        ranks = (row+1..current_row-1).map { |n| n = n }
       end
     end
-    game = self.game
     game.pieces.each do |p|
-      if ranks.include?(p.rank) && files.include?(p.file)
+      if files.include?(p.file) && ranks.include?(p.rank)
         return true
       end
     end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -6,47 +6,47 @@ RSpec.describe Piece, type: :model do
 
     before(:each) do
       @game = FactoryGirl.build(:game)
-      @current_square = FactoryGirl.create(:piece, rank: 4, file: 4, game: @game)    
+      @current_square = FactoryGirl.create(:piece, file: 4, rank: 4, game: @game)    
     end
     
     it "checks if there is no obstruction" do
-      obstruction = FactoryGirl.create(:piece, rank: 6, file: 6, game: @game)
+      obstruction = FactoryGirl.create(:piece, file: 6, rank: 6, game: @game)
       expect(@current_square.is_obstructed?(5, 5)).to be false
     end
 
     it "checks if there is an obstruction horizontally to the right" do
-      obstruction = FactoryGirl.create(:piece, rank: 4, file: 5, game: @game)
-      expect(@current_square.is_obstructed?(4, 6)).to be true
-    end
-    it "checks if there is an obstruction horizontally to the left" do
-      obstruction = FactoryGirl.create(:piece, rank: 4, file: 3, game: @game)
-      expect(@current_square.is_obstructed?(4, 2)).to be true
-    end
-
-    it "checks if there is an obstruction vertically above" do
-      obstruction = FactoryGirl.create(:piece, rank: 5, file: 4, game: @game)
+      obstruction = FactoryGirl.create(:piece, file: 5, rank: 4, game: @game)
       expect(@current_square.is_obstructed?(6, 4)).to be true
     end
-    it "checks if there is an obstruction vertically below" do
-      obstruction = FactoryGirl.create(:piece, rank: 3, file: 4, game: @game)
+    it "checks if there is an obstruction horizontally to the left" do
+      obstruction = FactoryGirl.create(:piece, file: 3, rank: 4, game: @game)
       expect(@current_square.is_obstructed?(2, 4)).to be true
     end
 
+    it "checks if there is an obstruction vertically above" do
+      obstruction = FactoryGirl.create(:piece, file: 4, rank: 5, game: @game)
+      expect(@current_square.is_obstructed?(4, 6)).to be true
+    end
+    it "checks if there is an obstruction vertically below" do
+      obstruction = FactoryGirl.create(:piece, file: 4, rank: 3, game: @game)
+      expect(@current_square.is_obstructed?(4, 2)).to be true
+    end
+
     it "checks if there is an obstruction diagonally top-right" do
-      obstruction = FactoryGirl.create(:piece, rank: 5, file: 5, game: @game)
+      obstruction = FactoryGirl.create(:piece, file: 5, rank: 5, game: @game)
       expect(@current_square.is_obstructed?(6, 6)).to be true
     end
     it "checks if there is an obstruction diagonally bottom-right" do
-      obstruction = FactoryGirl.create(:piece, rank: 3, file: 5, game: @game)
-      expect(@current_square.is_obstructed?(2, 6)).to be true
+      obstruction = FactoryGirl.create(:piece, file: 5, rank: 3, game: @game)
+      expect(@current_square.is_obstructed?(6, 2)).to be true
     end
     it "checks if there is an obstruction diagonally bottom-left" do
-      obstruction = FactoryGirl.create(:piece, rank: 3, file: 3, game: @game)
+      obstruction = FactoryGirl.create(:piece, file: 3, rank: 3, game: @game)
       expect(@current_square.is_obstructed?(2, 2)).to be true
     end
     it "checks if there is an obstruction diagonally top-left" do
-      obstruction = FactoryGirl.create(:piece, rank: 5, file: 3, game: @game)
-      expect(@current_square.is_obstructed?(6, 2)).to be true
+      obstruction = FactoryGirl.create(:piece, file: 3, rank: 5, game: @game)
+      expect(@current_square.is_obstructed?(2, 6)).to be true
     end
   end
 
