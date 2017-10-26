756c1cc26fdd7f92facdb905fea78955f49ecc75
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-26 10:24:11 +0000

    tests for show and update actions

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
+  describe "pieces#show" do
+    it "should successfully reload the board when clicking on a square" do
+      game = FactoryGirl.build(:game)
+      get :show, params: { id: game.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "pieces#update" do
+    it "should update the file and rank of the chess piece when moved" do
+      piece = FactoryGirl.create(:piece, file: 2, rank: 1)
+      patch :update, params: { id: piece.id, file: 3, rank: 3}
+      expect(piece.file).to eq 3
+      expect(piece.rank).to eq 3
+    end
+    # it "shouldn't let players who don't own a piece update it" do
+    #   piece = FactoryGirl.create(:piece, file: 2, rank: 1)
+    #   player = FactoryGirl.create(:user)
+    #   sign_in player
+    #   patch :update, params: { id: piece.id, file: 3, rank: 3}
+    #   expect(response).to have_http_status(:forbidden)
+    # end
+  end
 
 end
