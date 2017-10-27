e9039fdc3e9c139b26cc9e531c838aa2a32c4005
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-10-27 13:35:24 +0000

    current draft pieces#update action

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,12 +1,19 @@
 class PiecesController < ApplicationController
+before_action :authenticate_user!, only: :update
 
   def show
     @piece = Piece.find_by_id(params[:id])
+    if @piece.blank?
+      render plain: 'Not Found :(', status: :not_found
+    end
   end
 
   def update
     @piece = Piece.find_by_id(params[:id])
-    # return render_not_found(:forbidden) if @piece.user != current_user
+    return render_not_found if @piece.blank?
+    if @piece.user != current_user
+      return render plain: 'Forbidden :(', status: :forbidden
+    end
     @piece.update_attributes(piece_params)
     if @piece.valid?
       redirect_to game_path
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
   describe "pieces#show" do
-    it "should successfully reload the board when clicking on a square" do
-      piece = FactoryGirl.build(:piece)
+    it "should successfully load the view if a piece exists" do
+      piece = FactoryGirl.create(:piece)
       ## define click action??
       get :show, params: { id: piece.id }
       expect(response).to have_http_status(:success)
@@ -12,19 +12,26 @@ RSpec.describe PiecesController, type: :controller do
 
   describe "pieces#update" do
     it "should update the file and rank of the chess piece when moved" do
-      piece = FactoryGirl.create(:piece, file: 2, rank: 1)
+      piece = FactoryGirl.create(:piece)
+      sign_in piece.user
       patch :update, params: { id: piece.id, piece: { file: 3, rank: 3}}
       piece.reload
       expect(piece.file).to eq 3
       expect(piece.rank).to eq 3
     end
-    # it "shouldn't let players who don't own a piece update it" do
-    #   piece = FactoryGirl.create(:piece, file: 2, rank: 1)
-    #   player = FactoryGirl.create(:user)
-    #   sign_in player
-    #   patch :update, params: { id: piece.id, file: 3, rank: 3}
-    #   expect(response).to have_http_status(:forbidden)
-    # end
+    it "shouldn't let players who don't own a piece update it" do
+      piece = FactoryGirl.create(:piece)
+      player = FactoryGirl.create(:user)
+      sign_in player
+      patch :update, params: { id: piece.id, piece: { file: 3, rank: 3}}
+      expect(response).to have_http_status(:forbidden)
+    end
+    it "should create a move model on update" do
+
+    end
+    it "should redirect to the games#show once done" do
+
+    end
   end
 
 end
