155395cb203e371018168de3ddd79f49b600a6b6
Gary Ravago <ravagog@gmail.com>
2017-10-28 12:41:56 -0700

    Refactor populate_board unit test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,34 +3,25 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
 
   it ".populate_board" do
+    
     game = FactoryGirl.create(:game)
+
+    piece_positions = []
+    ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"].each.with_index(1) do |piece, i|
+      piece_positions << { type: piece,   file: i, rank: 1, game: game, user: game.white_player }
+      piece_positions << { type: piece,   file: i, rank: 8, game: game, user: game.black_player }
+    end
+
+    1.upto(8).each do |column|
+      piece_positions << { type: "Pawn", file: column, rank: 2, game: game, user: game.white_player }
+      piece_positions << { type: "Pawn", file: column, rank: 7, game: game, user: game.black_player }
+    end
 
     game.populate_board
 
-    expect(game.pieces.where(type:"Rook",   file:1, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Knight", file:2, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Bishop", file:3, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Queen",  file:4, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"King",   file:5, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Bishop", file:6, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Knight", file:7, rank:1, user: game.white_player).count).to eq(1)
-    expect(game.pieces.where(type:"Rook",   file:8, rank:1, user: game.white_player).count).to eq(1)
-
-    1.upto(8).each do |column|
-      expect(game.pieces.where(type:"Pawn",   file: column, rank:2, user: game.white_player).count).to eq(1)
-      expect(game.pieces.where(type:"Pawn",   file: column, rank:7, user: game.black_player).count).to eq(1)
+    piece_positions.each do |piece|
+      expect(Piece.exists?(piece)).to eq(true)
     end
-  
-    expect(game.pieces.where(type:"Rook",   file:1, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Knight", file:2, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Bishop", file:3, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Queen",  file:4, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"King",   file:5, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Bishop", file:6, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Knight", file:7, rank:8, user: game.black_player).count).to eq(1)
-    expect(game.pieces.where(type:"Rook",   file:8, rank:8, user: game.black_player).count).to eq(1)
-
-    expect(game.pieces.count).to eq(32)
   end
   
 end
