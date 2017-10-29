3c1f1607c16e86022bc3ed33d93d089eec9adaf3
Gary Ravago <ravagog@gmail.com>
2017-10-29 12:49:03 -0700

    Add additional test for .populate_board and adjust syntax

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,26 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
 
-  it ".populate_board" do
-    
-    game = FactoryGirl.create(:game)
+  describe ".populate_board" do
 
-    piece_positions = []
-    ["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"].each.with_index(1) do |piece, i|
-      piece_positions << { type: piece,   file: i, rank: 1, game: game, user: game.white_player }
-      piece_positions << { type: piece,   file: i, rank: 8, game: game, user: game.black_player }
+    before do
+      @game = FactoryGirl.create(:game)
     end
 
-    1.upto(8).each do |column|
-      piece_positions << { type: "Pawn", file: column, rank: 2, game: game, user: game.white_player }
-      piece_positions << { type: "Pawn", file: column, rank: 7, game: game, user: game.black_player }
+    it "creates pieces in specific positions" do
+      
+      piece_positions = []
+      ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook'].each.with_index(1) do |piece, i|
+        piece_positions << { type: piece,   file: i, rank: 1, game: @game, user: @game.white_player, color: 'white' }
+        piece_positions << { type: piece,   file: i, rank: 8, game: @game, user: @game.black_player, color: 'black' }
+      end
+
+      1.upto(8).each do |column|
+        piece_positions << { type: 'Pawn', file: column, rank: 2, game: @game, user: @game.white_player, color: 'white' }
+        piece_positions << { type: 'Pawn', file: column, rank: 7, game: @game, user: @game.black_player, color: 'black' }
+      end
+
+      @game.populate_board
+
+      piece_positions.each do |piece|
+        expect(Piece.exists?(piece)).to eq(true)
+      end
+
     end
 
-    game.populate_board
+    it "creates a specific number of types per color" do 
+      
+      number_of_chess_types = {
+        'Rook': 2,
+        'Knight': 2,
+        'Bishop': 2,
+        'Queen': 1,
+        'King': 1,
+        "Pawn": 8
+      }
 
-    piece_positions.each do |piece|
-      expect(Piece.exists?(piece)).to eq(true)
+      @game.populate_board
+
+      number_of_chess_types.each do |piece, number|
+        expect(Piece.where(type: piece, color:'white').count).to eq(number)
+        expect(Piece.where(type: piece, color:'black').count).to eq(number)
+      end
+      
     end
-  end
-  
+  end 
 end
