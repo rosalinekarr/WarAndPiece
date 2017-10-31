5a0a29c1c250398253712d0e9526e02d417c7db3
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-10-31 13:50:20 -0400

    Aligned grid neatly with chessboard, changed 'piece.subclasses' to 'piece.type'

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,5 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
 
-  
+  describe ".populate_board" do
+
+    before do
+      @game = FactoryGirl.create(:game)
+    end
+
+    it "creates pieces in specific positions" do
+      
+      piece_positions = []
+      ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook'].each.with_index(1) do |piece, i|
+        piece_positions << { type: piece,   file: i, rank: 1, game: @game, user: @game.white_player, color: 'white' }
+        piece_positions << { type: piece,   file: i, rank: 8, game: @game, user: @game.black_player, color: 'black' }
+      end
+
+      1.upto(8).each do |column|
+        piece_positions << { type: 'Pawn', file: column, rank: 2, game: @game, user: @game.white_player, color: 'white' }
+        piece_positions << { type: 'Pawn', file: column, rank: 7, game: @game, user: @game.black_player, color: 'black' }
+      end
+
+      @game.populate_board
+
+      piece_positions.each do |piece|
+        expect(Piece.exists?(piece)).to eq(true)
+      end
+
+    end
+
+    it "creates a specific number of types per color" do 
+      
+      number_of_chess_types = {
+        'Rook': 2,
+        'Knight': 2,
+        'Bishop': 2,
+        'Queen': 1,
+        'King': 1,
+        'Pawn': 8
+      }
+
+      @game.populate_board
+
+      number_of_chess_types.each do |piece, number|
+        expect(Piece.where(type: piece, color:'white').count).to eq(number)
+        expect(Piece.where(type: piece, color:'black').count).to eq(number)
+      end
+      
+    end
+  end 
 end
