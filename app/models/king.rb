04ee27da56bbc876f409a9dee2a434c16dc20b15
Renee Sandefur <reneersandefur@gmail.com>
2017-11-03 17:06:20 +0000

    refactored king model to be rubocop compliant

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,10 +1,11 @@
+# Logic for King piece
 class King < Piece
   # file and rank where piece is trying to move
   def valid_move?(new_file, new_rank)
     if super(new_file, new_rank) == false
       'Not Valid'
     # checks to make sure move is not greater than 1
-    elsif (new_file - self.file).abs <= 1 && (new_rank - self.rank).abs <= 1
+    elsif (new_file - file).abs <= 1 && (new_rank - rank).abs <= 1
       true
     else
       'Not Valid'
