18482c83cf499578ca50f3fa6bdabc2320416151
Renee Sandefur <reneersandefur@gmail.com>
2017-11-03 19:30:47 +0000

    made super logic for valid move cleaner

--- app/models/king.rb
+++ app/models/king.rb
@@ -2,10 +2,9 @@
 class King < Piece
   # file and rank where piece is trying to move
   def valid_move?(new_file, new_rank)
-    if super(new_file, new_rank) == false
-      'Not Valid'
+    return 'Not Valid' unless super(new_file, new_rank) == true
     # checks to make sure move is not greater than 1
-    elsif (new_file - file).abs <= 1 && (new_rank - rank).abs <= 1
+    if (new_file - file).abs <= 1 && (new_rank - rank).abs <= 1
       true
     else
       'Not Valid'
