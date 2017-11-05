1673c1800cab3e34212dddd60731673a0b784444
Renee Sandefur <reneersandefur@gmail.com>
2017-11-05 22:19:53 +0000

    refactored pawn model and started refactoring pawn spec

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -4,15 +4,19 @@ class Pawn < Piece
     white? && rank == 2 || black? && rank == 7
   end
 
+  def backward_move?(new_rank)
+    (black? && new_rank > rank) || (white? && new_rank < rank)
+  end
+
   def valid_move?(new_file, new_rank)
     return false unless super(new_file, new_rank)
     return false unless new_file == file
+    return false if backward_move?(new_rank)
 
     if first_move?
-      (black? && rank - new_rank == 1 || rank - new_rank == 2) ||
-        (white? && new_rank - rank == 1 || new_rank - rank == 2)
+      (new_rank - rank).abs <= 2
     elsif !first_move?
-      black? && (rank - new_rank) == 1 || white? && (new_rank - rank) == 1
+      (new_rank - rank).abs <= 1
     end
   end
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Pawn, type: :model do
-  
   describe 'pawn#valid_move? checks if the pawn is a valid move' do
     before(:each) do
       @game = FactoryGirl.build(:game)
     end
-        
+
     it 'should return true for valid forward move' do
-      @pawn = FactoryGirl.create(:pawn, file: 1, rank: 2, color: 'white', game: @game)
+      @pawn =
+        FactoryGirl.create(:pawn, file: 1, rank: 2, color: 'white', game: @game)
       expect(@pawn.valid_move?(1, 4)).to eq(true)
     end
-    it 'should return true for valid forward move' do
-      @pawn = FactoryGirl.create(:pawn, file: 1, rank: 2, color: 'white', game: @game)
-      expect(@pawn.valid_move?(1, 3)).to eq(true)
-    end
-    it 'should return false for valid forward move' do
-      @pawn = FactoryGirl.create(:pawn, file: 1, rank: 3, color: 'white', game: @game)
+    it 'should return false for invalid forward move' do
+      @pawn =
+        FactoryGirl.create(:pawn, file: 1, rank: 3, color: 'white', game: @game)
       expect(@pawn.valid_move?(1, 5)).to eq(false)
     end
     it 'should return false for invalid move back' do
-      @pawn = FactoryGirl.create(:pawn, file: 1, rank: 2, game: @game)
+      @pawn =
+        FactoryGirl.create(:pawn, file: 1, rank: 2, color: 'white', game: @game)
       expect(@pawn.valid_move?(1, 1)).to eq(false)
     end
     it 'should return true for valid move forward' do
-      @pawn = FactoryGirl.create(:pawn, file: 2, rank: 7, color: 'black', game: @game)
+      @pawn =
+        FactoryGirl.create(:pawn, file: 2, rank: 7, color: 'black', game: @game)
       expect(@pawn.valid_move?(2, 5)).to eq(true)
     end
-    it 'should return false for valid move forward' do
-      @pawn = FactoryGirl.create(:pawn, file: 2, rank: 7, color: 'black', game: @game)
+    it 'should return false for invalid move back' do
+      @pawn =
+        FactoryGirl.create(:pawn, file: 2, rank: 7, color: 'black', game: @game)
       expect(@pawn.valid_move?(2, 8)).to eq(false)
     end
     it 'should return false for move off board' do
