8c823057110f7790d9ca4992309e9d404536fdca
Renee Sandefur <reneersandefur@gmail.com>
2017-11-07 23:05:06 +0000

    updated static controller authentication

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
   
   def index
     @inprogress_games = Game.inprogress
