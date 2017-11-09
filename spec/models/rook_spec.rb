ae3b851801fe6777a1304e3b04d7a159bc4947b9
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-11-09 11:50:32 +0000

    tests

---
+++ spec/models/rook_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.describe Rook, type: :model do
+
+  describe 'Rook#valid_move? checks if the Rook is making a valid move' do
+    before(:each) do
+      @game = FactoryGirl.build(:game)
+      @rook = FactoryGirl.create(:piece, type: 'Rook', file: 1, rank: 1, game: @game)
+    end
+    it 'returns true for vertical move' do
+      expect(@rook.valid_move?(1, 6)).to be true
+    end
+    it 'returns true for horizontal move' do
+      expect(@rook.valid_move?(6, 1)).to be true
+    end
+    it 'returns false for diagonal move' do
+      expect(@rook.valid_move?(3, 3)).to be false
+    end
+    it 'returns false for a non-linear move' do
+      expect(@rook.valid_move?(7, 3)).to be false
+    end
+    it 'returns false for a move off board' do
+      expect(@rook.valid_move?(1, 9)).to be false
+    end
+  end
+end
