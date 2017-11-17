576b01ef2a1061cff137a153c0694429d738385a
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-11-17 10:14:55 +0000

    support tracking turn in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028202209) do
+ActiveRecord::Schema.define(version: 20171116160537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171028202209) do
     t.string "game_state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "turn", default: true
   end
 
   create_table "moves", force: :cascade do |t|
