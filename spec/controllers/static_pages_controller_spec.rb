9d5d159bc596bb85d6a179286a900e8391adf68a
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-11-17 16:39:12 +0000

    removed user authentication before accessing homepage

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe "static_pages#index action" do
-    it "should require player to be logged in" do
-      get :index 
-      expect(response).to redirect_to new_user_session_path
-    end
     it "should successfully show all current games" do
-      player = FactoryGirl.create(:user)
-      sign_in player
       get :index
       expect(response).to have_http_status(:success)
     end
