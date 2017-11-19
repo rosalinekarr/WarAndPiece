2d01adc662fe65677e494f224a59e0075f60dc3a
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-11-19 21:04:22 +0000

    removed test about showing games on homepage as we will not display them there

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  describe "static_pages#index action" do
-    it "should successfully show all current games" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 end
