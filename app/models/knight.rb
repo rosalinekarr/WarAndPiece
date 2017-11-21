f31bd09182b4f9e0f5326328231bda90991c7515
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-11-21 10:20:02 -0500

    Refactored knight valid_move? method

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,50 +1,12 @@
 class Knight < Piece
 
   def valid_move?(new_file, new_rank)
-    
-    # validate move is within game board
+    # checks to see if move is on the board
     return false if !super(new_file, new_rank)
-
-    move_up_left?(new_file, new_rank) ||
-    move_up_right?(new_file, new_rank) ||
-    move_down_left?(new_file, new_rank) ||
-    move_down_right?(new_file, new_rank) ||
-    move_left_up?(new_file, new_rank) ||
-    move_left_down?(new_file, new_rank) ||
-    move_right_up?(new_file, new_rank) ||
-    move_right_down?(new_file, new_rank)
+    # checks to see if change in rank is equal to change in file, or if either change in file or change in rank are 0.
+    return false if (new_file - self.file).abs == (new_rank - self.rank).abs || new_file - self.file == 0 || new_rank - self.rank == 0
+    # checks to see if new square is at an appropriate distance
+    new_file - self.file).abs <= 2 && (new_rank - self.rank).abs <= 2  
   end
-
-  # validate move position against all 8 possible moves
-  def move_up_left?(file, rank)
-    (self.rank + 2 == rank) && (self.file - 1 == file)
-  end
-
-  def move_up_right?(file, rank)
-    (self.rank + 2 == rank) && (self.file + 1 == file)
-  end
-
-  def move_down_left?(file, rank)
-    (self.rank - 2 == rank) && (self.file - 1 == file)
-  end
-
-  def move_down_right?(file, rank)
-    (self.rank - 2 == rank) && (self.file + 1 == file)
-  end
-
-  def move_left_up?(file, rank)
-    (self.file - 2 == file) && (self.rank + 1 == rank)
-  end
-
-  def move_left_down?(file, rank)
-    (self.file - 2 == file) && (self.rank - 1 == rank)
-  end
-
-  def move_right_up?(file, rank)
-    (self.file + 2 == file) && (self.rank + 1 == rank)
-  end
-
-  def move_right_down?(file, rank)
-    (self.file + 2 == file) && (self.rank - 1 == rank)
-  end
+  
 end
