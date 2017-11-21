d39482bebbd8be1694c014a21876115eea57f315
Gary Ravago <ravagog@gmail.com>
2017-11-21 01:24:44 -0800

    Implement units tests for king get_valid_moves method

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -27,9 +27,13 @@ RSpec.describe King, type: :model do
 
   describe '.get_valid_moves' do
 
+    before do
+      @game = FactoryGirl.create(:game)
+    end
+    
     context "no adjacent pieces" do
       it "returns all 8 valid king moves" do
-        @king = FactoryGirl.create(:king, file: 5, rank: 5)
+        @king = FactoryGirl.create(:king, file: 5, rank: 5, game: @game)
         @king_valid_moves = [ {4=>4}, {4=>5}, {4=>6}, {5=>4},
                               {5=>6}, {6=>4}, {6=>5}, {6=>6} ]
                               
@@ -40,12 +44,43 @@ RSpec.describe King, type: :model do
     end
 
     context "adjacent pieces" do
-      it "returns valid moves excluding same color piece positions"
-      it "returns valid moves when opposing pieces are adjacent"
+      it "returns valid moves excluding same color piece positions" do
+        @king = FactoryGirl.create(:king, file: 2, rank: 2, color: 'white', game: @game)
+        1.upto(3) {|n| FactoryGirl.create(:pawn, file: n, rank: 3, color: 'white', game: @game) }
+        @king_valid_moves = [ {1=>1}, {1=>2}, {2=>1}, {3=>1}, {3=>2} ]
+                              
+        result = @king.get_valid_moves
+
+        expect(result).to eq(@king_valid_moves)
+      end
+
+      it "returns valid moves when opposing pieces are adjacent" do
+        @king = FactoryGirl.create(:king, file: 2, rank: 2, color: 'white', game: @game)
+        [:knight, :pawn, :knight].each.with_index(1) do |piece_type,i|
+          FactoryGirl.create(piece_type, file: i, rank: 3, color: 'black', game: @game) 
+        end                                         
+        @king_valid_moves = [ {1=>1}, {1=>2}, {1=>3}, {2=>1},
+                              {2=>3}, {3=>1}, {3=>2}, {3=>3} ]
+                              
+        result = @king.get_valid_moves
+
+        expect(result).to eq(@king_valid_moves)
+      end
     end
       
     context "on game board edge" do
-      it "returns valid moves when on edges"
+      it "returns valid moves when on edges" do
+        @king = FactoryGirl.create(:king, file: 1, rank: 1, game: @game) 
+        @king_valid_moves = [ {1=>2}, {2=>1}, {2=>2} ]
+                              
+        result = @king.get_valid_moves
+
+        expect(result).to eq(@king_valid_moves)
+      end
+    end
+
+    context "moves that place king in check" do
+      it "returns valid moves that do not place king in check"
     end
     
   end
