fd792bf692f896c464c826f9aaeb3661610c6ff4
Gary Ravago <ravagog@gmail.com>
2017-11-24 01:00:08 -0800

    Add more logic to Game.checkmate? method incorporating obstruction behvaior

--- app/models/game.rb
+++ app/models/game.rb
@@ -30,20 +30,33 @@ class Game < ApplicationRecord
 
     checked_king = self.pieces.where(type:"King").where.not(color: attacking_piece.color).first
     king_moves = checked_king.get_valid_moves
-    checked_king_team_pieces = self.pieces.where(color: checked_king.color, is_captured: false)
+    checked_king_team_pieces = self.pieces.where(color: checked_king.color, is_captured: false).where.not(type:"King")
 
+    # check if defending team can capture attacking piece
     checked_king_team_pieces.each do |piece|
       return false if piece.valid_move?(attacking_piece.file, attacking_piece.rank)
     end
 
     attack_team_pieces = self.pieces.where(color: attacking_piece.color, is_captured: false)
     
+    # check if defending team can obstruct
+    check_between_path = checked_king.get_path_between(attacking_piece.file, attacking_piece.rank)
+    if check_between_path
+      puts "#{check_between_path.inspect}"
+      check_between_path.each do |position|
+        checked_king_team_pieces.each do |piece|
+          return false if piece.valid_move?(position[0], position[1])
+        end
+      end
+    end
+
+    # check if king can/cannot escape with his valid moves
     king_moves.each do |move|
       king_file = move[0]
       king_rank = move[1]
 
       move_intersects = false    
-      attack_team_pieces.each do |piece|
+      attack_team_pieces.each do |piece| 
         if piece.valid_move?(king_file, king_rank)
           move_intersects = true 
           break
