e4a03ab69c8191a5477d13179bb2d142da56e2fd
hereismyusername <my.e.address123@gmail.com>
2017-11-26 11:13:17 +0000

    add terms and conditions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'team', to: 'static_pages#team'
   get 'contact', to: 'static_pages#contact'
+  get 'terms', to: 'static_pages#terms'
   get 'challenge', to: 'static_pages#challenge'
   resources :games do
     post 'join'
