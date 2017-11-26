4780b240a06838a49d01110857aa9410f6627b54
Gary Ravago <ravagog@gmail.com>
2017-11-25 20:16:07 -0800

    Refactor move_into_check_position? method into better solution

--- app/models/king.rb
+++ app/models/king.rb
@@ -29,32 +29,53 @@ class King < Piece
   end
 
   def move_into_check_position?(column, row)
-    opposing_piece_adjacent = set_king_capture_piece_off_on(column, row)
+    
+    valid_moves_before_capturing = []
+    opposing_piece_adjacent = Piece.new
     opposing_team = self.game.pieces.where.not(color: self.color).where(is_captured: false)
     opposing_team.each do |piece|
       if piece.valid_move?(column, row)
-        set_king_capture_piece_off_on(column, row) if opposing_piece_adjacent
-        return true
+        valid_moves_before_capturing << [column, row]
+      end
+
+      if piece.file == column && piece.rank == row
+        opposing_piece_adjacent = piece
       end
     end
-    
-    set_king_capture_piece_off_on(column, row)
-    false
-  end
 
-  def set_king_capture_piece_off_on(column, row)
-    opposing_piece_adjacent = self.game.pieces.where(file: column, rank: row).where.not(color: self.color).first
+    valid_moves_after_capturing = []
+    unless opposing_piece_adjacent.type.nil?
+      puts "#{opposing_piece_adjacent.type} is potential king capture piece!"
+      opposing_piece_adjacent.is_captured = true
+      opposing_piece_adjacent.save
     
-    if opposing_piece_adjacent
-      if opposing_piece_adjacent.is_captured
-        opposing_piece_adjacent.is_captured = false
-      else  
-        opposing_piece_adjacent.is_captured = true
+      opposing_team = self.game.pieces.where.not(color: self.color).where(is_captured: false)
+      opposing_team.each do |piece|
+        if piece.valid_move?(column, row)
+          valid_moves_after_capturing << [column, row]
+        end
       end
+
+      opposing_piece_adjacent.is_captured = false
       opposing_piece_adjacent.save
     end
-    
-    opposing_piece_adjacent
+
+    puts ""
+    puts ""
+    puts "position at #{column}, #{row}"
+    puts "#{valid_moves_before_capturing} moves before capture state"
+    puts "#{valid_moves_after_capturing} moves after capture state"
+
+
+
+    if valid_moves_before_capturing.empty? && valid_moves_after_capturing.empty?
+      puts "king can escape here"
+      false
+    else
+      puts "king is in check here"
+      true
+    end
+
   end
 
 end
